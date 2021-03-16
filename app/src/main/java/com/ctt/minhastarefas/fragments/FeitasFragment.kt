46543994010c4ac2fdb89910b326e94b0beaa6a0
Ana Arine <analucia.arine@gmail.com>
2021-03-16 17:40:38 -0300

    Atualização Fragment Feitas

--- app/src/main/java/com/ctt/minhastarefas/fragments/FeitasFragment.kt
+++ app/src/main/java/com/ctt/minhastarefas/fragments/FeitasFragment.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ctt.minhastarefas.MainActivity.Companion.adapterFeitas
 import com.ctt.minhastarefas.R
 import com.ctt.minhastarefas.adapter.TarefasAdapter
 import com.ctt.minhastarefas.model.Tarefa
@@ -17,9 +18,9 @@ import com.ctt.minhastarefas.model.Tarefa
 class FeitasFragment : Fragment() {
 
     private val CICLO_VIDA = "CICLO_VIDA"
-    private lateinit var botaoCadastrar: Button
-    private lateinit var tituloTarefa: EditText
-    private lateinit var descricaoTarefa: EditText
+//    private lateinit var botaoCadastrar: Button
+//    private lateinit var tituloTarefa: EditText
+//    private lateinit var descricaoTarefa: EditText
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -33,12 +34,7 @@ class FeitasFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val listaFeitas: MutableList<Tarefa> = mutableListOf(
-                Tarefa("Fiz tudo", "Pra ficar mais forte"),
-                Tarefa("Fui na praia", "Pra tomar picolé")
-        )
         val rvFeitas = view.findViewById<RecyclerView>(R.id.listaFeitas)
-        val adapterFeitas = TarefasAdapter(listaFeitas)
         rvFeitas.adapter = adapterFeitas
         rvFeitas.layoutManager = LinearLayoutManager(requireContext())
     }
